d6fe68b6dcb5278a29d70111e16b03264c3e7f11
lidongnan <lidongnan@victorysoft.com.cn>
2020-07-10 16:21:09 +0800

    修改str to word 中请求超长的bug

--- src/main/java/org/word/controller/WordController.java
+++ src/main/java/org/word/controller/WordController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -71,7 +68,6 @@ public class WordController {
      * @param response
      */
     @ApiOperation(value = "将 swagger 文档一键下载为 doc 文档", notes = "", tags = {"Word"})
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "请求成功。")})
     @RequestMapping(value = "/downloadWord", method = {RequestMethod.GET})
     public void word(Model model, @ApiParam(value = "资源地址", required = false) @RequestParam(required = false) String url, HttpServletResponse response) {
         generateModelData(model, url, 0);
@@ -103,7 +99,6 @@ public class WordController {
      * @return
      */
     @ApiOperation(value = "将 swagger json文件转换成 word文档并下载", notes = "", tags = {"Word"})
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "请求成功。")})
     @RequestMapping(value = "/fileToWord", method = {RequestMethod.POST})
     public void getWord(Model model, @ApiParam("swagger json file") @Valid @RequestPart("jsonFile") MultipartFile jsonFile, HttpServletResponse response) {
         generateModelData(model, jsonFile);
@@ -119,9 +114,8 @@ public class WordController {
      * @return
      */
     @ApiOperation(value = "将 swagger json字符串转换成 word文档并下载", notes = "", tags = {"Word"})
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "请求成功。")})
     @RequestMapping(value = "/strToWord", method = {RequestMethod.POST})
-    public void getWord(Model model, @ApiParam("swagger json string") @Valid @RequestParam("jsonStr") String jsonStr, HttpServletResponse response) {
+    public void getWord(Model model, @ApiParam("swagger json string")  @RequestBody String jsonStr, HttpServletResponse response) {
         generateModelData(model, jsonStr);
         writeContentToResponse(model, response);
     }
